e4e815e779821fc488d883bc8194c51566078e60
Luis Angel Peña <lapena@mavi.mx>
2021-05-17 19:01:59 -0500

    feat:: Use webpack-dev-middleware

--- webpack.config.js
+++ webpack.config.js
@@ -7,18 +7,20 @@ module.exports = {
     index: './src/index.js',
     print: './src/print.js'
   },
-  devtool: 'inline-source-map', // Rastrear de que archivo viene el error.
+  devtool: 'inline-source-map', // Track the error line in file
   devServer: {
-    contentBase: './dist',
+    contentBase: './dist', // Indicate to the server where are the files to get the content
   },
   plugins: [
-    new HtmlWebpackPlugin({
+    new HtmlWebpackPlugin({ // Auto-Generates the index.html
       title: 'Development',
     })
   ],
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true
+    filename: '[name].bundle.js', // Identify different entry points
+    path: path.resolve(__dirname, 'dist'), // Idicate where the transpiled files are
+    clean: true, // Clean the dist folder
+    publicPath: '/'
+      // webpack-dev-middleware: make sure files are served correctly on localhost:3000
   }
 };
